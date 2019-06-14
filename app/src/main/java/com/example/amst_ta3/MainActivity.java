b696132af6b00973ee0006956b9356edca2963c9
Juan Pablo Cadena Aguilar <jpcadena@espol.edu.ec>
2019-06-14 11:30:52 -0500

    cambios de mi rama

--- app/src/main/java/com/example/amst_ta3/MainActivity.java
+++ app/src/main/java/com/example/amst_ta3/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //esto es un comentario para realizar un commit #2
     }
 }
